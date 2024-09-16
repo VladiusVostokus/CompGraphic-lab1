1b6dd8858f3a7f10ff0f560fa401732f3a803033
VladiusVostokus <kovalenkov222@gmail.com>
2024-09-16 20:54:24 +0300

    Create vertex and fragment shaders

--- draw.js
+++ draw.js
@@ -1,5 +1,19 @@
 'use strict';
 
+const vsSource = `
+#version 300 es
+
+void main() {
+    gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+}`;
+
+const fsSource = `
+#version 300 es
+
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}`;
+
 function main() {
     const canvas = document.querySelector("#glcanvas");
     const gl = canvas.getContext("webgl");
