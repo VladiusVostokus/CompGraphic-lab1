8d9864781f056256b367e78e881048f78bba200f
VladiusVostokus <kovalenkov222@gmail.com>
2024-09-16 20:49:57 +0300

    Create and use program

--- draw.js
+++ draw.js
@@ -1,7 +1,5 @@
 'use strict';
 
-main();
-
 function main() {
     const canvas = document.querySelector("#glcanvas");
     const gl = canvas.getContext("webgl");
@@ -9,6 +7,10 @@ function main() {
         console.log("Failde to get context for WebGL");
         return;
     }
+    const program = gl.createProgram();
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.useProgram(program);
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
+
